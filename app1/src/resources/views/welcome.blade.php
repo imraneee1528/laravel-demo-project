4412471f3b929c3887ee5d42f0915246e2786f6c
imraneee1528 <imraneee1528@gmail.com>
2022-10-03 07:55:05 +0000

    new update

---
+++ app1/src/resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>App 1 </title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+        <div>
+            <h1>Hello I am App 1</h1>
+        </div>
+    </body>
+</html>
